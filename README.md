559e7a726b278c597987e8eb2264f441c5f47f4e
g44 <goldfourfour@gmail.com>
2019-02-25 01:50:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Concepto
+> Demonstrate the concept of modern JavaEE project structure for those that are not ready for microservice but would like to still prepare for it.
+
+**Technologies Used**
+- SpringBoot (JPA, Thymeleaf, etc.)
+- Maven multi-module
+- MySQL
+
+## Table Of Contents
+
+## More ...
+
+## License
---
+++ cc-guapo/src/main/java/me/patrick/guapo/GuapoApplication.java
@@ -0,0 +1,13 @@
+package me.patrick.guapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuapoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuapoApplication.class, args);
+    }
+
+}
